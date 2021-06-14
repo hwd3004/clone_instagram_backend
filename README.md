2590f5aeb26459c9dcfeeb5a037babcc152d8f01
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2021-06-14 20:43:18 +0900

    4.17 File Upload part Four

--- README.md
+++ README.md
@@ -303,3 +303,34 @@ Promise {
    ```
 
    node_moudles 폴더를 지우고 npm install
+
+---
+
+# 4.17 File Upload part Four
+
+```
+import { createWriteStream } from 'fs'
+
+...
+
+// node.js 명령어로, 호출한 프로젝트 디렉토리의 절대경로를 알려줌
+console.log(process.cwd())
+
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword, bio, avatar },
+  { loggedInUser },
+) => {
+  const { filename, createReadStream } = await avatar
+
+  // 모든 파일을 변수 readStream에 받음
+  const readStream = createReadStream()
+
+  // uploads 폴더에 파일을 생성할 수 있지만, graphql 서버는
+  // graphql만 담당하므로 apollo-server-express가 필요
+  const writeStream = createWriteStream(process.cwd() + '/uploads/' + filename)
+  readStream.pipe(writeStream)
+
+  ...
+
+```
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,6 +1,12 @@
+// File System. 파일을 처리하는 node.js 기본 라이브러리
+import { createWriteStream } from 'fs'
+
 import bcrypt from 'bcrypt'
 import client from '../../client'
 import { protectedResolver } from '../users.utils'
+
+// node.js 명령어로, 호출한 프로젝트 디렉토리의 절대경로를 알려줌
+console.log(process.cwd())
 
 const resolverFn = async (
   _,
@@ -9,9 +15,13 @@ const resolverFn = async (
 ) => {
   const { filename, createReadStream } = await avatar
 
-  // 모든 파일을 변수 stream에 받음
-  const stream = createReadStream()
-  console.log(stream)
+  // 모든 파일을 변수 readStream에 받음
+  const readStream = createReadStream()
+
+  // uploads 폴더에 파일을 생성할 수 있지만, graphql 서버는
+  // graphql만 담당하므로 apollo-server-express가 필요
+  const writeStream = createWriteStream(process.cwd() + '/uploads/' + filename)
+  readStream.pipe(writeStream)
 
   let uglyPassword = null
   if (newPassword) {
