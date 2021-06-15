a9eee07f81606641d67ebe8844af9b43514f2734
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2021-06-15 22:54:15 +0900

    4.29 코드 챌린지

--- users/searchUsers/searchUsers.resolvers.js
+++ users/searchUsers/searchUsers.resolvers.js
@@ -1,5 +1,6 @@
 import client from '../../client'
 
+/**
 export default {
   Query: {
     searchUsers: async (_, { keyword }) =>
@@ -10,5 +11,42 @@ export default {
           },
         },
       }),
+  },
+}
+**/
+
+export default {
+  Query: {
+    searchUsers: async (_, { keyword, page }) => {
+      const countNum = 3
+
+      if (!page) {
+        page = 1
+      }
+
+      const searchedUsers = await client.user.findMany({
+        where: {
+          username: {
+            startsWith: keyword.toLowerCase(),
+          },
+        },
+
+        take: countNum,
+        skip: (page - 1) * countNum,
+      })
+
+      const countSearchedUsers = await client.user.count({
+        where: {
+          username: {
+            startsWith: keyword.toLowerCase(),
+          },
+        },
+      })
+
+      return {
+        searchedUsers,
+        totalPages: Math.ceil(countSearchedUsers / 3),
+      }
+    },
   },
 }
--- users/searchUsers/searchUsers.typeDefs.js
+++ users/searchUsers/searchUsers.typeDefs.js
@@ -1,7 +1,20 @@
 import { gql } from 'apollo-server'
 
+/**
 export default gql`
   type Query {
     searchUsers(keyword: String!): [User]
+  }
+`
+**/
+
+export default gql`
+  type searchUsersResult {
+    searchedUsers: [User]
+    totalPages: Int
+  }
+
+  type Query {
+    searchUsers(keyword: String!, page: Int): searchUsersResult!
   }
 `
